82cc9b3c639d97c0f079c78ad7f89225e12ed00d
Abdelazizmostafa <abdoelflal93@gmail.com>
2019-06-26 03:24:46 +0300

    update contact us - partners

--- js/plugins.js
+++ js/plugins.js
@@ -8,13 +8,52 @@ $(document).ready(function(){
      $(this).parent().addClass("active").siblings().removeClass("active");  
     }); 
     
-    //Set input focus
+    //Set input focus newsletter
     $("#typename").on("focus",function(){
        $(this).removeAttr("placeholder");
      }); 
    $("#typename").on("blur",function(){
      $(this). attr("placeholder","Enter Your Email Address");
 
-}); 
+    });
+    
+    //Set input focus contact username
+    $("#username").on("focus",function(){
+       $(this).removeAttr("placeholder");
+     }); 
+   $("#username").on("blur",function(){
+     $(this). attr("placeholder","Enter Your Name");
+
+    }); 
+    
+    
+    //Set input focus contact useremail
+    $("#useremail").on("focus",function(){
+       $(this).removeAttr("placeholder");
+     }); 
+   $("#useremail").on("blur",function(){
+     $(this). attr("placeholder","Enter Your Email address");
+
+    });  
+    
+    
+    //Set input focus contact userphone
+    $("#userphone").on("focus",function(){
+       $(this).removeAttr("placeholder");
+     }); 
+   $("#userphone").on("blur",function(){
+     $(this). attr("placeholder","Enter Your Cell Phone");
+
+    });
+    
+    
+    //Set input focus contact userMess
+    $("#userMess").on("focus",function(){
+       $(this).removeAttr("placeholder");
+     }); 
+   $("#userMess").on("blur",function(){
+     $(this). attr("placeholder","Your Message");
+
+    }); 
     
 });
